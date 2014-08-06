7b2093c53f86d55aec5ad6746770678eed57383d
Estella <EstellaCarp@gmail.com>
2014-08-06 16:41:27 -0600

    update 36

---
+++ app/Repository/DBinput.php
@@ -0,0 +1,82 @@
+<?php 
+namespace Repository;
+
+use Project;
+use Validator;
+use Input;
+use Redirect;
+
+
+
+class DBinput implements DBinputInterface{
+
+	public function inputData(){
+
+		$validation = Validator:: make(Input::all(), Project::$rules);
+
+			if ($validation->fails()) {
+				return Redirect::back()->withInput()->withErrors($validation->messages());
+			}
+			$projects = new Project;
+			$projects->Title = Input::get('Title');
+			$projects->Description = Input::get('Description');
+			$projects->Lang = Input::get('Lang');
+			$projects->Link= Input::get('Link');
+
+			if (Input::hasFile('ImageBig')) {
+				$img = Input::file('ImageBig');
+				$nameBig = $projects->Title.'big';
+				$nameSmall= $projects->Title.'small';
+				$projects->ImageBig = $nameBig;
+				$projects->ImageSmall= $nameSmall;
+
+				$imageObjs = \Image::make($img);
+				$imageObjs->resize(200, null, function ($constraint) {
+			    $constraint->aspectRatio();
+				})->save(public_path().'/asset/image/'.$nameSmall);
+		
+				$imageObjb = \Image::make($img);
+				$imageObjb->resize(500, null, function ($constraint) {
+			    $constraint->aspectRatio();
+				})->save(public_path().'/asset/image/'.$nameBig);
+
+			}
+
+			$projects->save();
+			
+
+	}
+
+
+	public function UpdateData($id){
+
+			$projects= Project::find($id);
+			$projects->Title = Input::get('Title');
+			$projects->Description = Input::get('Description');
+			$projects->Lang = Input::get('Lang');
+			$projects->Link= Input::get('Link');
+
+			if (Input::hasFile('ImageBig')) {
+				$img = Input::file('ImageBig');
+				$nameBig = $projects->Title.'big';
+				$nameSmall= $projects->Title.'small';
+				$projects->ImageBig = $nameBig;
+				$projects->ImageSmall= $nameSmall;
+
+				$imageObjs = \Image::make($img);
+				$imageObjs->resize(200, null, function ($constraint) {
+				    $constraint->aspectRatio();
+					})->save(public_path().'/asset/image/'.$nameSmall);
+
+				$imageObjb = \Image::make($img);
+				$imageObjb->resize(500, null, function ($constraint) {
+				    $constraint->aspectRatio();
+					})->save(public_path().'/asset/image/'.$nameBig);
+
+			}
+
+			$projects->save();
+	}
+
+
+}
---
+++ app/Repository/DBinputInterface.php
@@ -0,0 +1,8 @@
+<?php namespace Repository;
+
+
+interface DBinputInterface {
+
+	public function inputData();
+
+}
